6d536d5f1773e319c2ced183079f05e3c1db8cff
Anibal C <detops.tk@gmail.com>
2015-11-16 13:52:16 -0500

    integration bototnes

--- public/js/js.js
+++ public/js/js.js
@@ -21,13 +21,12 @@
 		dom_id_form_respuesta: '#form-respuesta',
 	};
 
-
-
  	var App = {
 		version: '1.1',
 		localData: [],
 		indice: 0, // escenario
         indiceEvidencia: 0, // evidencia
+        indiceForm: 0,
 
 		init : function() {
 			console.log("loadData()");
@@ -44,43 +43,84 @@
 		* cargar datos by API
 		*/
 		loadData : function() {
-			var me = this; 
+			var me = this;
 	        $.getJSON( VARS.api_url, function( data ) {
 	        	$(VARS.dom_id_preload).hide(); 
 				me.localData = data;
+				me.reformatData();
+
                 console.log('data cargado!..', me.localData);
 				me.swichEscenario();
         	});
 		},
-		
+        reformatData: function() {
+        	var me = this;
+
+        	 //shuffle(this.localData);
+
+        	/**
+			* Recorrer todo los items y establecer respuesta correcta
+			* respuesta = true
+        	*/
+			$.each(me.localData, function(index1, value1) {
+
+				$.each(value1.data_evidencia,function(index2, value2) {
+
+					$.each(value2.data_formulario, function(index3, value3) {
+
+						if (value2.respuesta == value3.id_alternativa) {
+							value3.respuesta = true;
+						} else {
+							value3.respuesta = false;
+						}
+					});
+				});
+			});
+        },
         // PLAY ESCENARIO : cambiar de escenario segun indice
 		swichEscenario: function() {
 			var me = this;
-			if (me.localData.length > 0) {
-				if (me.indice > (me.localData.length-1)) {
+			var data = me.localData;
+			var indice = me.indice;
+
+			if (data.length > 0) {
+				if (indice > (data.length-1)) {
 					alert("juego se termino! " + indice)
 				} else {
-					me.escenarioCargarData(me.indice, me.localData[me.indice]);
-					me.escenarioIniciarJuego(me.indice, me.localData[me.indice]);
+					me.escenarioCargarData(indice, data[indice]);
+					me.escenarioIniciarJuego(indice, data[indice]);
 				}
 			} else {
 				alert("no existe data Escenario");
 			}
-
-			console.log('this.localData', this.localData)
 		},
 		// Escenario : pintar todo el escenario
 		escenarioCargarData: function(indice, data) {
 			var me = this;
 			var indiceMas1 = indice + 1;
+			var indiceForm = me.indiceForm;
+			var dataForm = data.data_evidencia[indiceForm];
 			// cambiar data escenario.
 			$(VARS.dom_id_escenario_titulo).text('Escenario '+ indiceMas1);
+            cargarBotones(data.data_evidencia[indiceForm]);
 
+            /**
+			* mostrar botones
+            */
+            function cargarBotones(dataEvidencia) {
+				var row = '{{for data_formulario}}<div class="row"><div class="col-md-12"><button class="col-md-12 col-sm-12 col-xs-12 btn btn-lg margin-bottom-5" data-respuesta="{{:respuesta}}">{{:~upper(description)}}</button></div></div>{{/for}}';
+				var tmpl = $.templates(row);
+				var tmplHtml = tmpl.render(dataEvidencia, myHelpers);
+				$(VARS.dom_id_form_opcion_respuesta).html(tmplHtml);
+            }
 		},
 		escenarioIniciarJuego: function(indice, data) {
             var me = this;
 			// escenario sound
             me.helpPlayAudioEscenario(data.path_audio, data.data_evidencia);
+
+  
+
 
 		},
 		
@@ -112,6 +152,9 @@
 			// escenario sound
             me.helpPlayAudioEvidencia(data.audio);
         },
+        
+        
+        
         
 
 		/*
